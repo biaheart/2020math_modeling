5257ce09ec56a2167805becb298f392b2f406b32
biaheart <44721134+biaheart@users.noreply.github.com>
2020-06-24 09:30:58 +0800

    第一次作业的指纹识别部分

---
+++ 数学建模数据处理/preprocess_test.py
@@ -0,0 +1,27 @@
+import os
+import numpy as np
+from openpyxl import Workbook
+# 读取txt文件
+directory = './test/'
+sample_doc_name=os.listdir(directory)
+sample_doc_name.sort(key= lambda x:int(x[4:len(x)-4]))  # 将文件名的数字提取出来进行排序
+data=()
+for file in sample_doc_name:
+    temp_data=[]
+    with open(directory+file,'r') as f:
+        for line in f:
+            temp=list(map(float,line.split()))
+            if temp:
+                temp_data+=temp
+    data+=(temp_data,)
+wb=Workbook()
+ws=wb.active
+independ_variable=np.linspace(501,7999,3750)/100
+for i in range(len(independ_variable)):
+    ws.cell(row=i+1,column=1,value=independ_variable[i])
+for i in range(len(data)):
+    temp=data[i][1::2]
+    for j in range(len(temp)):
+        ws.cell(row=j+1,column=i+2,value=temp[j])
+# 存储的文件名可以改一下
+wb.save('test.xlsx')
